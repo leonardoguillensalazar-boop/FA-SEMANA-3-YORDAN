4067f3a04de5a9178db387d784d295ee21ba5e16
leonardo <leonardoguillensalazar@gmail.com>
2025-09-02 12:06:59 -0500

    subiendo ejercicio 1

---
+++ SEMAN 3 YORDAN/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMAN_3_YORDAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey(); //detenimiento de consola
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmo de la carrera {carrera}");
+        }
+
+        static void ejer2()
+        {
+        }
+
+        static void ejer3()
+        {
+        }
+
+        static void ejer4()
+        {
+        }
+
+        static void ejer5()
+        {
+        }
+
+        static void ejer6()
+        {
+        }
+
+    }
+}
---
+++ SEMAN3 YORDAN_PYTHON/SEMAN3_YORDAN_PYTHON.py
@@ -0,0 +1,7 @@
+﻿def ejer1():
+    nombre = input("Ingrese su nombre: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, Bienvenido al curso de Fundamentos de Algoritmo de la carrera{carrera}")
+
+ejer1()
